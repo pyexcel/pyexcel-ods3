c28e58d63a42a975cc3251cd6635545f42ecfd49
chfw <wangc_2011@hotmail.com>
2016-05-14 23:32:48 +0100

    sync with commons

--- test.bat
+++ test.bat
@@ -1,2 +1,2 @@
 pip freeze
-nosetests --with-cov --cov pyexcel_ods3 --cov tests --with-doctest --doctest-extension=.rst tests README.rst pyexcel_ods3
+nosetests --with-cov --cover-package pyexcel_ods3 --cover-package tests --with-doctest --doctest-extension=.rst tests README.rst pyexcel_ods3
